19719b1562b7bc431948c4e4a9f7a669d5146169
JinHyeon <tiemo0708@naver.com>
2025-06-07 19:05:06 +0900

    refactor: toString 제거

--- src/main/java/com/example/demo/service/ReviewConsumer.java
+++ src/main/java/com/example/demo/service/ReviewConsumer.java
@@ -21,18 +21,18 @@ public class ReviewConsumer {
     public void consume(ReviewEvent event) {
         System.out.println("Kafka message consumed: " + event);
         MealReview review = new MealReview();
-        review.setMealdayInfo(event.getMealdayInfo().toString());
-        review.setMealType(event.getMealType().toString());
-        review.setOverallOpinion(event.getOverallOpinion() != null ? event.getOverallOpinion().toString() : null);
-        review.setFreeOpinion(event.getFreeOpinion() != null ? event.getFreeOpinion().toString() : null);
+        review.setMealdayInfo(event.getMealdayInfo());
+        review.setMealType(event.getMealType());
+        review.setOverallOpinion(event.getOverallOpinion() != null ? event.getOverallOpinion() : null);
+        review.setFreeOpinion(event.getFreeOpinion() != null ? event.getFreeOpinion() : null);
         mealReviewRepository.save(review);
 
         event.getMenuRatings().forEach((menu, rating) -> {
             MenuRating r = new MenuRating();
-            r.setMenuName(menu.toString());
+            r.setMenuName(menu);
             r.setRating(rating);
-            r.setQuestion(event.getMenuQuestions().get(menu).toString());
-            r.setAnswer(event.getMenuAnswers().get(menu).toString());
+            r.setQuestion(event.getMenuQuestions().get(menu));
+            r.setAnswer(event.getMenuAnswers().get(menu));
             r.setReview(review);
             menuRatingRepository.save(r);
             System.out.println("Saved MenuRating: " + r.getMenuName() + ", rating=" + r.getRating());
